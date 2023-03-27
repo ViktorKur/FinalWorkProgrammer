137bca4035b3e64138a784a8752b4af9d9715045
Viktor K <popvik13@mail.ru>
2023-03-27 14:28:01 +0300

    Sozdal i Dobavil v papku DZ_Work001 fail ZapolnenieMassiva.cs soderjashii kod zapolneniay massiva strokovymi dannymi

---
+++ ZapolnenieMassiva.cs
@@ -0,0 +1,9 @@
+// Функция заполнение массива arreyStr строковыми данными и возврат массива вызывающей функции
+public static class ZapolnenieMassiva
+{
+  public static string[] ZapolnenieArray()
+  {
+    string[] arreyStr = new string[] { "hello", "2", "world", ":)", "1234", "1567", "-2", "computer science", "KAZAN", "DENMARK", "Russian"};
+    return arreyStr;
+  }
+}

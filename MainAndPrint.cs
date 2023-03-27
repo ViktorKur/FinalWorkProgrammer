fa727fd409b20f4f9aa93e500d6c4c32029001dd
Viktor K <popvik13@mail.ru>
2023-03-27 14:24:09 +0300

    Sozdal i Dobavil v papku DZ_Work001 fail MainAndPrint.cs soderjashii osnovnyi zaprosy i vvyvod rezultata na pechat

---
+++ MainAndPrint.cs
@@ -0,0 +1,10 @@
+﻿using static ZapolnenieMassiva;
+using static PreobrazovanArr;
+Console.Clear();
+string[] strArr = ZapolnenieArray(); // Запрос через функцию ZapolnenieArray ономерного строковаго массива 
+string[] arrayDOThreeChar = strArr.PreobrazovanArrTOThreeChar(); // Присвоение массиву rrayDOThreeChar
+//  результата выполнения функции PreobrazovanArrTOThreeChar
+Console.WriteLine($"Заданный строковый массив: [" + string.Join(", ", strArr) + "]"); // Вывод исходного массива
+Console.WriteLine($"Массив после выборки из заданного массива с длиной символов не больше 3х: [" 
++ string.Join(", ", arrayDOThreeChar)  + "]"); // Вывод массива после выборки
+Console.WriteLine();
